693eac937e13679eb03ff53031849dd3d55b2a7e
Andrew Hazel <Andrew.Hazel@manchester.ac.uk>
2010-05-10 14:59:30 +0000

    Removed a number of Ctrl-M's from the drivers in the curved_pipe demo driver
    Added axisymmetric advection-diffusion equations in 
    spherical polar coordinates and an "eluting sphere"
    demo_driver in Refineable and non-Refineable forms.
    
    
    
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@120 595e1d82-adf1-4282-bac7-b6073fa2ca8e

---
+++ demo_drivers/spherical_advection_diffusion/eluting_sphere/validate.sh
@@ -0,0 +1,111 @@
+#! /bin/sh
+
+
+#Set the number of tests to be checked
+NUM_TESTS=2
+
+
+# Setup validation directory
+#---------------------------
+touch Validation
+rm -r -f Validation
+mkdir Validation
+
+# Validation for adaptive spin up
+#--------------------------------
+cd Validation
+mkdir RESLT
+mkdir RESLT_adapt
+
+echo "Running spherical polar eluting sphere validation "
+
+../eluting_sphere > OUTPUT_eluting_sphere
+echo "done"
+echo " " >> validation.log
+echo "Spherical polar eluting sphere validation" >> validation.log
+echo "---------------------------------------------" >> validation.log
+echo " " >> validation.log
+echo "Validation directory: " >> validation.log
+echo " " >> validation.log
+echo "  " `pwd` >> validation.log
+echo " " >> validation.log
+cat  RESLT/soln10.dat > el_sphere.dat
+
+if test "$1" = "no_fpdiff"; then
+  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+else
+../../../../bin/fpdiff.py ../validata/el_sphere.dat.gz  \
+         el_sphere.dat 0.1 1.0e-14 >> validation.log
+fi
+
+
+echo "Running adaptive spherical polar eluting spehre validation "
+../eluting_sphere_adapt > OUTPUT_eluting_sphere_adapt
+echo "done"
+echo " " >> validation.log
+echo "Adaptive spherical polar eluting sphere validation" >> validation.log
+echo "---------------------------------------------------" >> validation.log
+echo " " >> validation.log
+echo "Validation directory: " >> validation.log
+echo " " >> validation.log
+echo "  " `pwd` >> validation.log
+echo " " >> validation.log
+cat  RESLT_adapt/soln10.dat > el_sphere_adapt.dat
+
+if test "$1" = "no_fpdiff"; then
+  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+else
+../../../../bin/fpdiff.py ../validata/el_sphere_adapt.dat.gz  \
+         el_sphere_adapt.dat 0.1 1.0e-14 >> validation.log
+fi
+
+# Append log to main validation log
+cat validation.log >> ../../../../validation.log
+
+cd ..
+
+
+
+#######################################################################
+
+
+#Check that we get the correct number of OKs
+OK_COUNT=`grep -c 'OK' Validation/validation.log`
+if  [ $OK_COUNT -eq $NUM_TESTS ]; then
+ echo " "
+ echo "======================================================================"
+ echo " " 
+ echo "All tests in" 
+ echo " " 
+ echo "    `pwd`    "
+ echo " "
+ echo "passed successfully."
+ echo " "
+ echo "======================================================================"
+ echo " " 
+else
+  if [ $OK_COUNT -lt $NUM_TESTS ]; then
+   echo " "
+   echo "======================================================================"
+   echo " " 
+   echo "Only $OK_COUNT of $NUM_TESTS test(s) passed; see"
+   echo " " 
+   echo "    `pwd`/Validation/validation.log"
+   echo " " 
+   echo "for details" 
+   echo " " 
+   echo "======================================================================"
+   echo " "
+  else 
+   echo " "
+   echo "======================================================================"
+   echo " " 
+   echo "More OKs than tests! Need to update NUM_TESTS in"
+   echo " " 
+   echo "    `pwd`/validate.sh"
+   echo " "
+   echo "======================================================================"
+   echo " "
+  fi
+fi
+
