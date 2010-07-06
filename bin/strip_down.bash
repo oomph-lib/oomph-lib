80e50c6a6d2255563c1946f43389c388a24c0dcf
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2010-07-06 08:14:24 +0000

    A few additional files for generation of stripped out version.
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@136 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- bin/strip_down.bash
+++ bin/strip_down.bash
@@ -44,51 +44,78 @@ echo " "
 echo " Do you want to strip down this distribution [y/n -- default: n]"
 reply=`OptionRead`
 if test "$reply" != "y" -a "$reply" != "Y" ; then 
-    echo "not doing it"
+    echo "Not doing it..."
     exit 0
 else
-    echo "doing it"
-    exit 0
+    echo "Doing it..."
 fi
 
-# Delete command
+
+# Backup directory with overwritten config files
+if [ -e backed_up_stripped_out_files ]
+then
+    echo " "
+    echo "ERROR: Directory "
+    echo " " 
+    echo "       backed_up_stripped_out_files"
+    echo " "
+    echo "already exists. Please delete it and try again."
+    echo " "
+    exit 1
+else
+    mkdir backed_up_stripped_out_files
+fi
+
+
+# Fake delete/copy commands
 del="echo Deleting:  `ls -d`"
+cop="echo I would overwrite these files: `ls -d`"
+
+# Real delete/copy commands
+del="rm -rf "
+cop="cp "
 
 # Generic top level stuff
 #========================
 
 # Update makefiles
-cp $home_dir/config/stripped_down_files/Makefile.am.top_level Makefile.am
-cp $home_dir/config/stripped_down_files/Makefile.am.bin bin/Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makefile.am.top_level 
+$cop $home_dir/config/stripped_down_files/Makefile.am.top_level Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.bin
+$cop $home_dir/config/stripped_down_files/Makefile.am.bin bin/Makefile.am
+
 
 # Get rid of user src and drivers
+echo "Deleting user_src and user-drivers"
 $del user_src
 $del user_drivers
 
 # Kill private directory
+echo "Deleting private"
 $del private
 
 # Get rid of entire doc directory
+echo "Deleting doc"
 $del doc
-
 
 
 
 # Self tests: Only keep analyse stage
 #====================================
+echo "Wiping most of self_test"
 cd self_test
 mv analyse_self_tests ../tmp_analyse_self_tests
 $del *
 mv ../tmp_analyse_self_tests analyse_self_test
-cp $home_dir/config/stripped_down_files/Makefile.am.self_test Makefile.am
-
-
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.self_test
+$cop $home_dir/config/stripped_down_files/Makefile.am.self_test Makefile.am
 
 
 
 # Demo drivers
 #=============
 cd $home_dir
+echo "Wiping most of demo drivers"
 cd demo_drivers
 
 
@@ -99,7 +126,8 @@ mv mpi ../tmp_mpi
 $del *
 mv ../tmp_poisson poisson
 mv ../tmp_mpi mpi
-cp $home_dir/config/stripped_down_files/Makefile.am.demo_drivers Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.demo_drivers
+$cop $home_dir/config/stripped_down_files/Makefile.am.demo_drivers Makefile.am
 
 
 # Within Poisson get rid of everything apart from adaptive flux bc
@@ -108,7 +136,8 @@ cd poisson
 mv two_d_poisson_flux_bc_adapt ../tmp_two_d_poisson_flux_bc_adapt
 $del *
 mv ../tmp_two_d_poisson_flux_bc_adapt two_d_poisson_flux_bc_adapt 
-cp $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.poisson Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.demo_drivers.poisson
+$cop $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.poisson Makefile.am
 cd ..
 
 
@@ -118,13 +147,15 @@ cd mpi
 mv distribution ../tmp_distribution
 $del *
 mv ../tmp_distribution distribution
-cp $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.mpi Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.demo_drivers.mpi
+$cop $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.mpi Makefile.am
 
 cd distribution
 mv two_d_poisson_flux_bc_adapt ../tmp_two_d_poisson_flux_bc_adapt
 $del * 
 mv ../tmp_two_d_poisson_flux_bc_adapt two_d_poisson_flux_bc_adapt
-cp $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.mpi.distribution Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.demo_drivers.mpi.distribution
+$cop $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.mpi.distribution Makefile.am
 
 
 
@@ -132,7 +163,7 @@ cp $home_dir/config/stripped_down_files/Makefile.am.demo_drivers.mpi.distributio
 # Sources
 #========
 cd $home_dir/src
-
+echo "Wiping most of src"
 
 # Keep only Poisson, generic and meshes
 #--------------------------------------
@@ -143,7 +174,8 @@ $del *
 mv ../tmp_generic generic
 mv ../tmp_poisson poisson
 mv ../tmp_meshes  meshes
-cp $home_dir/config/stripped_down_files/Makefile.am.src Makefile.am
+$cop Makefile.am backed_up_stripped_out_files/Makfile.am.src
+$cop $home_dir/config/stripped_down_files/Makefile.am.src Makefile.am
 
 
 # Keep only rectangular quad meshes
@@ -155,8 +187,10 @@ cp rectangular_quadmesh.template.cc simple_rectangular_quadmesh.template.cc ../t
 $del *.h *.cc
 mv ../tmp_meshes/* .
 rmdir ../tmp_meshes
-cp $home_dir/config/stripped_down_files/mesh_names.list .
-cp $home_dir/config/stripped_down_files/Makefile.am.meshes Makefile.am
+$cop mesh_names.list backed_up_stripped_out_files/mesh_names.list
+$cop $home_dir/config/stripped_down_files/mesh_names.list .
+$cop Makefile.am backed_up_stripped_out_files/Makefile.am.meshes
+$cop $home_dir/config/stripped_down_files/Makefile.am.meshes Makefile.am
 
 
 
@@ -164,12 +198,32 @@ cp $home_dir/config/stripped_down_files/Makefile.am.meshes Makefile.am
 
 # Update configure scripts
 #=========================
+echo "Updating configure scripts"
+
 cd $home_dir
 cd config/configure.ac_scripts
-cp $home_dir/config/stripped_down_files/user_drivers.dir_list .
-cp $home_dir/config/stripped_down_files/user_src.dir_list .
-cp $home_dir/config/stripped_down_files/core.dir_list .
-cp $home_dir/config/stripped_down_files/doc.dir_list .
+
+
+$cop private.dir_list backed_up_stripped_out_files/private.dir_list 
+$cop $home_dir/config/stripped_down_files/private.dir_list .
+
+$cop private_user_drivers.dir_list backed_up_stripped_out_files/private_user_drivers.dir_list 
+$cop $home_dir/config/stripped_down_files/private_user_drivers.dir_list .
+
+$cop private_user_src.dir_list backed_up_stripped_out_files/private_user_src.dir_list 
+$cop $home_dir/config/stripped_down_files/private_user_src.dir_list .
+
+$cop user_drivers.dir_list backed_up_stripped_out_files/user_drivers.dir_list 
+$cop $home_dir/config/stripped_down_files/user_drivers.dir_list .
+
+$cop user_src.dir_list backed_up_stripped_out_files/user_src.dir_list 
+$cop $home_dir/config/stripped_down_files/user_src.dir_list .
+
+$cop core.dir_list backed_up_stripped_out_files/core.dir_list 
+$cop $home_dir/config/stripped_down_files/core.dir_list .
+
+$cop doc.dir_list backed_up_stripped_out_files/doc.dir_list 
+$cop $home_dir/config/stripped_down_files/doc.dir_list .
 
 
 
