cf9fa676749618e0f4dca7b2d0ffa18e46959fbd
Raymon White <mly@595e1d82-adf1-4282-bac7-b6073fa2ca8e>
2013-12-09 08:53:21 +0000

    Updated validata for matrix_addition self tests
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@583 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- self_test/mpi/matrix_addition/matrix_addition.cc
+++ self_test/mpi/matrix_addition/matrix_addition.cc
@@ -58,161 +58,11 @@ void output_vector(Vector<myType> &given_vector)
   }
 }
 
-// Given the number of blocks (in both dimensions), and an array containing
-// the dimensions.
-// Return: mat_info_vec, which contains the dimensions of the sub blocks 
-// in a coherent manner. The number of global rows and columns of the block i,j
-// is represented as: mat_info_vec[i][j][0] and mat_info_vec[i][j][1]
-// respectively.
-void fill_in_mat_info(unsigned const nblock_row, unsigned const nblock_col, 
-                      unsigned dimarray[], 
-                      Vector<Vector<Vector<unsigned> > > &mat_info_vec)
-{
-  // The total number of dimensions is two times the number of blocks
-  // Since matrices are squares. This should be the same number of elements in
-  // dimarray.
-  unsigned ndims = 2*nblock_row*nblock_col;
-  
-  // Put dimarray into dimvec_all, since Vectors are nicer to work with.
-  Vector<unsigned> dimvec_all;
-  construct_vector(dimarray,ndims,dimvec_all);
-  
-  // index for the dimvec_all array.
-  unsigned dimvec_all_i = 0;
-  for (unsigned block_row_i = 0; block_row_i < nblock_row; block_row_i++) 
-  {
-    // create the Vector for the columns for the current row.
-    Vector<Vector<unsigned> > current_block_col_vec;    
-     
-    // loop through the block columns
-    for (unsigned block_col_i = 0; block_col_i < nblock_col; block_col_i++) 
-    {
-      // loop through the dimensions for this block.
-      Vector<unsigned> current_dim_vec;
-       
-      for (unsigned dim_i = 0; dim_i < 2; dim_i++) 
-      {
-        current_dim_vec.push_back(dimvec_all[dimvec_all_i]);
-        dimvec_all_i++;
-      } // for the individual block dimensions.
-
-      // push current_dim_vec into the current entry...
-      current_block_col_vec.push_back(current_dim_vec);
-    } // for the columns in this block row.
-
-    // push the current block column vec onto the current row vec.
-    mat_info_vec.push_back(current_block_col_vec);
-  }// for the number of block rows.
-}
-
-// Given the dimensions of a matrix, returns a matrix where the elements 
-// (starting from the number 1) are ascending per column, per row. 
-// I.e., for a 3 by 3 matrix we have:
-// [1 2 3
-//  4 5 6
-//  7 8 9]
-void create_matrix_ascend_col_row(unsigned const nrow, unsigned const ncol,
-                                  const OomphCommunicator* const comm_pt,
-                                  bool const distributed, 
-                                  CRDoubleMatrix &block)
-{  
-  // Clear the block
-  block.clear();
-  
-  // Create the distribution.
-  LinearAlgebraDistribution distri(comm_pt,nrow,distributed);
-
-  // The number of rows this processor is responsible for.
-  unsigned nrow_local = distri.nrow_local();
-  
-  // The number of values this processor will have to insert.
-  unsigned nval = nrow_local*ncol;
-  
-  // The first_row will be used as an offset for the values to insert.
-  unsigned first_row = distri.first_row();
-
-  // Fill in values...
-  Vector<double> values(nval,0);
-  for (unsigned val_i = 0; val_i < nval; val_i++)
-  {
-    values[val_i] = (val_i+1) + (first_row)*ncol;
-  }
-  
-  // Vectors for the column index and row_start.
-  Vector<int> column_indicies(nval,0);
-  Vector<int> row_start(nrow_local+1,0);
-
-  unsigned column_indicies_i = 0;
-  for (unsigned row_i = 0; row_i < nrow_local; row_i++)
-  {
-    for (unsigned col_i = 0; col_i < ncol; col_i++) 
-    {
-      column_indicies[column_indicies_i] = col_i;
-      column_indicies_i++;
-    }
-    row_start[row_i] = ncol*row_i;
-  }
-
-  // Fill in the last row_start.
-  row_start[nrow_local] = nval;
-
-  block.build(&distri,ncol,values,column_indicies,row_start);
-}
-
-
-// Given mat_info_vec, which describes the sub blocks as so:
-// The number of global rows and columns of the block i,j
-// is represented as: mat_info_vec[i][j][0] and mat_info_vec[i][j][1]
-// respectively. Pointers to the uniformly distribted matrices is returned.
-void fill_in_sub_matrices(const OomphCommunicator* const comm_pt, 
-                          const bool distributed,
-                          Vector<Vector<Vector<unsigned> > > &mat_info,
-                          DenseMatrix<CRDoubleMatrix* > &mat_pt)
-{
-  unsigned nblock_row = mat_pt.nrow();
-  unsigned nblock_col = mat_pt.ncol();
-
-  for (unsigned block_row_i = 0; block_row_i < nblock_row; block_row_i++) 
-  {
-    for (unsigned block_col_i = 0; block_col_i < nblock_col; block_col_i++) 
-    {
-      unsigned nrow = mat_info[block_row_i][block_col_i][0];
-      unsigned ncol = mat_info[block_row_i][block_col_i][1];
-      
-      mat_pt(block_row_i,block_col_i)=new CRDoubleMatrix;
-
-      create_matrix_ascend_col_row(nrow,ncol,comm_pt,distributed,
-                                   *mat_pt(block_row_i,block_col_i));
-    }
-  }
-}
-
-// Helper funcion to call other helper functions
-// 1) fill_in_mat_info(...) to fill in a data structure describing the matrices
-// 2) fill_in_sub_matrices(...) uses the mat_info to fill in the sub matrices 
-void create_matrices_to_cat
-(const unsigned nblock_row, const unsigned nblock_col, unsigned dimarray[],
- const OomphCommunicator* const comm_pt, DenseMatrix<CRDoubleMatrix*>&mat_pt)
-{
-  // We store the above array in a data structure such that
-  // mat_info[0][0][0] gives us the number of rows in the block (0,0)
-  // mat_info[0][0][1] gives us the number of columns in the block (0,0)
-  Vector<Vector<Vector<unsigned> > > mat_info_vec;
-
-  // Helper function to fill in mat_info given
-  // nblock_row, nblock_col and dim_array.
-  fill_in_mat_info(nblock_row,nblock_col,dimarray,mat_info_vec);
-  
-  // Fill in each sub matrix using create_matrix_ascend_col_row(..)
-  bool distributed = true;
-  fill_in_sub_matrices(comm_pt,distributed,mat_info_vec,mat_pt);
-}
-
-//===start_of_main======================================================
-/// Driver code: Testing CRDoubleMatrixHelpers::concatenation(...)
+//===start_of_main=============================================================
+/// Driver code: Testing CRDoubleMatrixHelpers::add(...)
 ///
 /// The script validate.sh should run this self test on 1, 2, 3 and 4 cores.
-//======================================================================
+//=============================================================================
 int main(int argc, char* argv[])
 {
 #ifdef OOMPH_HAS_MPI
@@ -262,67 +112,84 @@ int main(int argc, char* argv[])
   //
 
   // First matrix (mat_zero)
-  unsigned nrow_global_zero = 5;
-  unsigned ncol_zero = 5;
-  unsigned nnz_zero = 9;
-  Vector<double> val_zero;
-  Vector<int> col_i_zero;
-  Vector<int> row_s_zero;
+  unsigned nrow_global_t1m1 = 5;
+  unsigned ncol_t1m1 = 5;
+  unsigned nnz_t1m1 = 9;
+  Vector<double> val_t1m1;
+  Vector<int> col_i_t1m1;
+  Vector<int> row_s_t1m1;
 
-  double val_array_zero[] = {1,2,3,4,5,6,7,8,9};
-  int col_i_array_zero[] = {0,1,0,2,3,4,0,1,4};
-  int row_s_array_zero[] = {0,2,4,6,9,9};
+  double val_array_t1m1[] = {1,2,3,4,5,6,7,8,9};
+  int col_i_array_t1m1[] = {0,1,0,2,3,4,0,1,4};
+  int row_s_array_t1m1[] = {0,2,4,6,9,9};
 
-  construct_vector(val_array_zero,nnz_zero,val_zero);
-  construct_vector(col_i_array_zero,nnz_zero,col_i_zero);
-  construct_vector(row_s_array_zero,nrow_global_zero+1,row_s_zero);
+  construct_vector(val_array_t1m1,nnz_t1m1,val_t1m1);
+  construct_vector(col_i_array_t1m1,nnz_t1m1,col_i_t1m1);
+  construct_vector(row_s_array_t1m1,nrow_global_t1m1+1,row_s_t1m1);
 
-  LinearAlgebraDistribution distri_zero(comm_pt,nrow_global_zero,true);
+  LinearAlgebraDistribution distri_zero(comm_pt,nrow_global_t1m1,true);
 
-  CRDoubleMatrix mat_zero;
+  CRDoubleMatrix mat_t1m1;
   CRDoubleMatrixHelpers::create_uniformly_distributed_matrix(
-      nrow_global_zero,ncol_zero,comm_pt,
-      val_zero,col_i_zero,row_s_zero,mat_zero);
+      nrow_global_t1m1,ncol_t1m1,comm_pt,
+      val_t1m1,col_i_t1m1,row_s_t1m1,mat_t1m1);
 
-  std::ostringstream mat_zero_stream;
-  mat_zero_stream << "mat_zero_NP"<<nproc<<"R"<<my_rank;
-  mat_zero.sparse_indexed_output(mat_zero_stream.str());
+  std::ostringstream mat_t1m1_stream;
+  mat_t1m1_stream << "t1m1_NP"<<nproc<<"R"<<my_rank;
+  mat_t1m1.sparse_indexed_output(mat_t1m1_stream.str());
 
-  // Next matrix (mat_one)
-  unsigned nrow_global_one = 5;
-  unsigned ncol_one = 5;
-  unsigned nnz_one = 18;
-  Vector<double> val_one;
-  Vector<int> col_i_one;
-  Vector<int> row_s_one;
+  // Next matrix (mat_t1m2)
+  unsigned nrow_global_t1m2 = 5;
+  unsigned ncol_t1m2 = 5;
+  unsigned nnz_t1m2 = 18;
+  Vector<double> val_t1m2;
+  Vector<int> col_i_t1m2;
+  Vector<int> row_s_t1m2;
 
-  double val_array_one[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
-  int col_i_array_one[] = {0,1,2,3,4,0,1,2,3,4,2,4,0,1,2,2,3,4};
-  int row_s_array_one[] = {0,5,10,12,15,18};
+  double val_array_t1m2[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18};
+  int col_i_array_t1m2[] = {0,1,2,3,4,0,1,2,3,4,2,4,0,1,2,2,3,4};
+  int row_s_array_t1m2[] = {0,5,10,12,15,18};
 
-  construct_vector(val_array_one,nnz_one,val_one);
-  construct_vector(col_i_array_one,nnz_one,col_i_one);
-  construct_vector(row_s_array_one,nrow_global_one+1,row_s_one);
+  construct_vector(val_array_t1m2,nnz_t1m2,val_t1m2);
+  construct_vector(col_i_array_t1m2,nnz_t1m2,col_i_t1m2);
+  construct_vector(row_s_array_t1m2,nrow_global_t1m2+1,row_s_t1m2);
 
-  LinearAlgebraDistribution distri_one(comm_pt,nrow_global_one,true);
+  LinearAlgebraDistribution distri_t1m2(comm_pt,nrow_global_t1m2,true);
 
-  CRDoubleMatrix mat_one;
+  CRDoubleMatrix mat_t1m2;
   CRDoubleMatrixHelpers::create_uniformly_distributed_matrix(
-      nrow_global_one,ncol_one,comm_pt,
-      val_one,col_i_one,row_s_one,mat_one);
+      nrow_global_t1m2,ncol_t1m2,comm_pt,
+      val_t1m2,col_i_t1m2,row_s_t1m2,mat_t1m2);
 
-  std::ostringstream mat_one_stream;
+  std::ostringstream mat_t1m2_stream;
 
-  mat_one_stream << "mat_one_NP"<<nproc<<"R"<<my_rank;
-  mat_one.sparse_indexed_output(mat_one_stream.str());
+  mat_t1m2_stream << "t1m2_NP"<<nproc<<"R"<<my_rank;
+  mat_t1m2.sparse_indexed_output(mat_t1m2_stream.str());
 
-  CRDoubleMatrix mat_result;
+  CRDoubleMatrix mat_t1_result;
 
-  mat_zero.add(mat_one,mat_result);
+  mat_t1m1.add(mat_t1m2,mat_t1_result);
 
-  std::ostringstream mat_result_stream;
-  mat_result_stream << "mat_result_NP"<<nproc<<"P"<<my_rank;
-  mat_result.sparse_indexed_output(mat_result_stream.str());
+  std::ostringstream mat_t1_result_stream;
+  mat_t1_result_stream << "t1_res_NP"<<nproc<<"P"<<my_rank;
+  mat_t1_result.sparse_indexed_output(mat_t1_result_stream.str());
+
+
+  // Now test with the following matrices: A + B = C where
+  // A = [1  2  3  4  5  6
+  //      7  8  9  10 11 12
+  //      13 14 15 16 17 18]
+  //
+  // B = [1  2  3  4  5  6
+  //      7  8  9  10 11 12
+  //      13 14 15 16 17 18]
+  //
+  // B = [2  4  6  8  10 12
+  //      14 16 18 20 22 24
+  //      26 28 30 32 34 36]
+  //
+  
+
 
 
   //  // The number of block rows and columns, these will be set to the correct
