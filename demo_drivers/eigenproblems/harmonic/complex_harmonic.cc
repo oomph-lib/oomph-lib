4566605eb2e69c78fda75393cd34037663666f7a
Andrew Hazel <Andrew.Hazel@manchester.ac.uk>
2014-06-18 12:40:08 +0000

    Fixed tolerance problem with new eigenproblem test
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@712 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- demo_drivers/eigenproblems/harmonic/complex_harmonic.cc
+++ demo_drivers/eigenproblems/harmonic/complex_harmonic.cc
@@ -63,10 +63,11 @@ public:
  bool operator()(const complex<T> &x, const complex<T> &y) const
   {
    //Return the order in terms of magnitude if they are not equal
-   if(std::abs(x) != std::abs(y))
+   //Include a tolerance to avoid processor specific ordering
+   if(std::abs(std::abs(x) - std::abs(y)) > 1.0e-10 )
     {return std::abs(x) < std::abs(y);}
-   //Otherwise sort on real part
-   else if(x.real() != y.real())
+   //Otherwise sort on real part, again with a tolerance
+   else if(std::abs(x.real() - y.real()) > 1.0e-10)
     {return x.real() < y.real();}
    //Otherwise sort on imaginary part
    else 
