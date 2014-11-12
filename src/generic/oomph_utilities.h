648e274bb638005dd3b6af44c61acb16494fe095
David Shepherd <dshepherd@595e1d82-adf1-4282-bac7-b6073fa2ca8e>
2014-11-12 17:18:20 +0000

    Add prettyprint header to use for later c++ standards
    * prettyprint98.h is not forwards compatible.
    * Added prettyprint.h which is forwards compatible, and added ifdef to
      automatically include the correct header depending on version.
    * Also added some comments about the origin of these headers.
    
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@847 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- src/generic/oomph_utilities.h
+++ src/generic/oomph_utilities.h
@@ -55,8 +55,15 @@
 #include "oomph_definitions.h"
 #include "communicator.h"
 
-//header for cxx-prettyprint: A C++ Container Pretty-Printer
-#include "prettyprint98.h"
+// Header for cxx-prettyprint: A C++ Container Pretty-Printer. If the
+// standard in use is older than C++11 then use a special older version of
+// the header. Header is from here:
+// http://louisdx.github.io/cxx-prettyprint/
+#if __cplusplus <= 199711L
+  #include "prettyprint98.h"
+#else
+  #include "prettyprint.h"
+#endif
 
 // Header for name demangling
 #include <cxxabi.h>
