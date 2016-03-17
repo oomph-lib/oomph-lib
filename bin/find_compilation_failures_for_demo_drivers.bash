5b6179c7cd79337e012986296dc349e7763e3bc8
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2016-03-17 11:18:26 +0000

    -- Deleted another accidentally checked in Makefile.in.
    -- Changed Makefile.am in demo_drivers/eigenproblem demo drivers so  that
       executables never get declared as build targets if we don't have
       arpack. SUBDIRS variable in the directory above already bypasses
       these directories, but we visit them (by  other means) when checking
       for unbuilt executables -- these are usually interpreted as compilation
       failures. 
    
    -- Also had to modify bin/find_compilation_failures_for_demo_drivers.bash
       and top level Makefile.am to deal (in a shockingly hacky way)
       with the fact that when the script is called during the normal self-test
       process, its call to make happens within an already ongoing make 
       operation; this creates lots of extra output that I now filter out... Best 
       not to look how... 
    
    
    
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@1141 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- bin/find_compilation_failures_for_demo_drivers.bash
+++ bin/find_compilation_failures_for_demo_drivers.bash
@@ -8,14 +8,23 @@
 # could go unobserved in a "make -k check" based self test.
 #===================================================================
 
+
 # Where are we?
 current_dir=`pwd`
 
 # Check if we're running mpi self-tests
-mpi_run_command_length=`make do-we-have-mpi`
+mpi_run_command_length_aux=`make do-we-have-mpi`
+
+#echo "mpi_run_command_length_aux: "$mpi_run_command_length_aux
+
+# Strip out the potential additional on-screen output that is generated
+# by calling this from within make.
+mpi_run_command_length=`echo $mpi_run_command_length_aux | awk '{first=match($0, "START_BLA")+9; second=match($0, "END_BLA"); lngth=second-first; print substr($0, first, lngth)}'`
+
 echo "mpi_run_command_length: "$mpi_run_command_length
+
 have_mpi=0
-if [ "$mpi_run_command_length" -ne 0 ]; then
+if [ "$mpi_run_command_length" != "0" ]; then
     have_mpi=1
     echo "Have mpi compiler..."
 else
