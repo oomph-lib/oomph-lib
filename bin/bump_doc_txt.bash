cdba3952cdc513d3d33c2d2caa8c756cd0b26365
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2016-11-16 11:27:53 +0000

    Updated bump_doc_txt.bash so that it also bumps doc/the_distribution/the_distribution.txt. Other files can be added to the list at the beginning of this file (though, currently this doesn't seem necessary since no other *.txt files in doc contain the svn revision related strings that need to be updated during commit).
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@1268 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- bin/bump_doc_txt.bash
+++ bin/bump_doc_txt.bash
@@ -1,35 +1,47 @@
 #! /bin/bash
 
-echo " " 
-echo "Bumping revision information in doc.txt by adding dummy comment "
-echo "at end of file..."
-echo " "
 
+files_to_be_bumped="doc/doc.txt doc/the_distribution/the_distribution.txt"
+for file in `echo $files_to_be_bumped`; do
 
-if [ ! -e doc/doc.txt ]; then
-    echo "File doc/doc.txt not found. This script should be run "
+    echo " " 
+    echo "Bumping revision information in "
+    echo " " 
+    echo "     $file "
+    echo " " 
+    echo "by adding dummy comment at end of file."
+    echo " "
+    
+if [ ! -e $file ]; then
+    echo "File $file not found. This script should be run "
     echo "from oomph-lib home directory!"
     exit 1
 else
     # Check that doc.txt does not have any other changes so far
-    bla=`svn diff doc/doc.txt | wc -c`
+    bla=`svn diff $file | wc -c`
     if [ $bla -ne 0 ]; then
         echo " " 
         echo "ERROR:" 
         echo " " 
-        echo "doc/doc.txt has already been changed; please commmit these"
+        echo "$file has already been changed; please commmit these"
         echo "changes first. The changes are:"
         echo " " 
-        svn diff doc/doc.txt
+        svn diff $file
         echo " " 
         exit 1
     else
-        # Bump and auto-commit
-        cd doc; echo "#dummy" > junk.txt; cat doc.txt junk.txt > doc2.txt; mv doc2.txt doc.txt; rm junk.txt
-        svn commit doc.txt -m "Bumped doc/doc.txt revision number by running bin/bump_doc_txt.bash"
+        # Bump
+        echo "#dummy" > junk.txt; cat $file junk.txt > tmp.txt; mv tmp.txt $file; rm junk.txt
     fi
 
 fi
 
+
+done
+
+# If we're still going here commit
+echo "About to commit..."
+svn commit $files_to_be_bumped -m "Bumped revision numbers in $files_to_be_bumped by running bin/bump_doc_txt.bash"
 echo "...done"
 
+exit
