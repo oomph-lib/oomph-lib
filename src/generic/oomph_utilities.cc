4927a9710fb8f287e1987b453121dc9cedc6597b
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2017-07-27 07:03:47 +0000

    Black box Newton solver in oomph_utilities.cc kept outputting the Jacobian; possibly uncommented by somebody while debugging something. Commented out again.
    git-svn-id: svn+ssh://vummath.ma.man.ac.uk/home/svn-oomphlib/public/trunk@1295 595e1d82-adf1-4282-bac7-b6073fa2ca8e

--- src/generic/oomph_utilities.cc
+++ src/generic/oomph_utilities.cc
@@ -311,9 +311,9 @@ void black_box_fd_newton_solve(ResidualFctPt residual_fct,
      unknowns[i]=backup;
     }
 
-   oomph_info << "\n\nJacobian: "<< std::endl;
-   jacobian.sparse_indexed_output(std::cout);
-   oomph_info << std::endl;
+   // oomph_info << "\n\nJacobian: "<< std::endl;
+   // jacobian.sparse_indexed_output(std::cout);
+   // oomph_info << std::endl;
 
    // Get gradient
    if (Use_step_length_control)
