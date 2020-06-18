e45b1ff17f57a3c9759efc3ebc6fe27638331a7a
jondea <jondea@thedeakin.net>
2020-06-18 11:41:13 +0100

    Add lines to delete problematic files in website_install script

--- bin/website_install.sh
+++ bin/website_install.sh
@@ -8,6 +8,10 @@ mkdir -p $build_dir
 # Copy across all the stuff we need
 cp -a bin config demo_drivers doc external_src self_test src user_src -t $build_dir
 
+# Delete some problematic sfiles
+rm -f $build_dir/bin/fig2poly
+rm -f $build_dir/bin/create_fluid_and_solid_surface_mesh_from_fluid_xda_mesh
+
 # Move gitignore
 cp website.gitignore $build_dir/.gitignore
 
