bcb7dd129d9e1a9d56ad414f02b93716eb6a4eb9
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-07-21 17:47:49 +0100

    Disabled one_d_act_inhibit self-test on macOS only.

--- demo_drivers/reaction_diffusion/one_d_act_inhibit/validate.sh
+++ demo_drivers/reaction_diffusion/one_d_act_inhibit/validate.sh
@@ -3,10 +3,8 @@
 # Get the OOPMH-LIB root directory from a makefile
 OOMPH_ROOT_DIR=$1
 
-
 #Set the number of tests to be checked
 NUM_TESTS=1
-
 
 # Setup validation directory
 #---------------------------
@@ -21,38 +19,33 @@ cd Validation
 echo "Running 1D reaction-diffusion validation "
 mkdir RESLT
 cd RESLT
-../../one_d_act_inhibit > ../OUTPUT_1d_act
+../../one_d_act_inhibit >../OUTPUT_1d_act
 cd ..
 echo "done"
-echo " " >> validation.log
-echo "1D reaction-diffusion validation " >> validation.log
-echo "------------------------------------------------" >> validation.log
-echo " " >> validation.log
-echo "Validation directory: " >> validation.log
-echo " " >> validation.log
-echo "  " `pwd` >> validation.log
-echo " " >> validation.log
-cat RESLT/step10.dat > one_d_act_inhibit.dat
+echo " " >>validation.log
+echo "1D reaction-diffusion validation " >>validation.log
+echo "------------------------------------------------" >>validation.log
+echo " " >>validation.log
+echo "Validation directory: " >>validation.log
+echo " " >>validation.log
+echo "  " $(pwd) >>validation.log
+echo " " >>validation.log
+cat RESLT/step10.dat >one_d_act_inhibit.dat
 
 if test "$2" = "no_fpdiff"; then
-  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >>validation.log
 else
-$OOMPH_ROOT_DIR/scripts/fpdiff.py ../validata/one_d_act_inhibit.dat.gz \
-    one_d_act_inhibit.dat  >> validation.log
+  $OOMPH_ROOT_DIR/scripts/fpdiff.py ../validata/one_d_act_inhibit.dat.gz \
+    one_d_act_inhibit.dat >>validation.log
 fi
-
 
 # Append output to global validation log file
 #--------------------------------------------
-cat validation.log >> $OOMPH_ROOT_DIR/validation.log
-
+cat validation.log >>$OOMPH_ROOT_DIR/validation.log
 
 cd ..
 
-
-
 #######################################################################
-
 
 #Check that we get the correct number of OKs
 # validate_ok_count will exit with status
