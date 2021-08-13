1aa10c9f2a0f761908d588f827d6a29cef08f701
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-08-13 09:12:10 +0100

    Simplify copyright notice update script significantly.

--- bin/update_copyright_licence_header.sh
+++ bin/update_copyright_licence_header.sh
@@ -1,48 +1,32 @@
-#! /bin/sh
+#!/usr/bin/env bash
 
 # exit when any command fails
 set -e
 
 #-------------------------------------------------------------------------------
-# Shell script to perform the following steps (in the described order):
-# 1. Remove licence (all lines starting with //LIC//) from all oomph-lib *.h
-#    and *.cc files.
-# 2. Update end-year of the copyright licence in "cc_and_h_licence_block.txt".
-# 3. Apply licence prefix to all oomph-lib *.h and *.cc files.
+# Shell script to update the copyright notice of all oomph-lib-owned files.
 #-------------------------------------------------------------------------------
 
-# Find *.h and *.cc files in oomph-lib distribution (i.e. exclude external_src)
-oomph_lib_h_and_cc_files=$(find demo_drivers src user_src user_drivers self_test bin \( -name '*.h' -o -name '*.cc' \) -exec ls {} \;)
+# --------[ IDENTIFY COPYRIGHTED FILES ]----------------------------------------
 
-# --------[ REMOVE LICENSE ]----------------------------------------------------
+# Get the current filename; we need to ignore it from the search rule
+current_filename=$(basename "$0")
 
-# Remove the licence header from each source file
-for file in $oomph_lib_h_and_cc_files; do
-    echo "Delicencing:" $file
-    gawk -f bin/remove_licence.awk $file >$file.delicenced
-    mv -f $file.delicenced $file
-done
+# Search for all files containing the copyright notice
+oomph_lib_copyrighted_files=$(grep . -R -l --exclude="$current_filename" -e 'Copyright (C) 2006-' -e ' Matthias Heil and Andrew Hazel')
 
 # --------[ UPDATE COPYRIGHT YEAR ]---------------------------------------------
 
+# The end-year to update the notice to
 current_year=$(date +"%Y")
-sed --in-place "s/Copyright (C) 2006-.* Matthias Heil and Andrew Hazel/Copyright (C) 2006-$current_year Matthias Heil and Andrew Hazel/g" bin/cc_and_h_licence_block.txt
 
-# --------[ APPLY LICENSE ]-----------------------------------------------------
-
-# Add the licence header back to delicenced files
-for file in $oomph_lib_h_and_cc_files; do
-    echo "Licencing:" $file
-    cat bin/cc_and_h_licence_block.txt $file >$file.licenced
-    gawk -f bin/remove_licence.awk $file.licenced >$file.delicenced
-    if (test $(diff $file $file.delicenced | wc -w) != 0); then
-        echo "Original and licenced/delicenced file don't match!"
-        echo "Please run bin/remove_licence.sh first."
-        echo "Filename: " $file
-        exit 1
-    else
-        echo "OK"
-        mv -f $file.licenced $file
-        rm $file.delicenced
-    fi
+# Update the licence header for each file. Don't use the "-i" flag for an
+# in-place edit as this flag is not supported by the BSD version of "sed" that
+# ships with macOS
+for file in $oomph_lib_copyrighted_files; do
+    echo "Updating copyright notice for file:" $file
+    sed "s/Copyright (C) 2006-.* Matthias Heil and Andrew Hazel/Copyright (C) 2006-$current_year Matthias Heil and Andrew Hazel/g" $file >$file.updated
+    mv $file.updated $file
 done
+
+# ------------------------------------------------------------------------------
