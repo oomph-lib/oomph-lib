54e503c0c82d4124d31093a47d980231f768cfbf
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-08-30 12:39:19 +0100

    Patch incorrect deletion of matrix arrays in preconditioner_array.cc.

--- src/generic/preconditioner_array.cc
+++ src/generic/preconditioner_array.cc
@@ -902,7 +902,7 @@ namespace oomph
           nnz_recv[i][p] = nnz_recv_temp[(i * nproc) + p];
         }
       }
-      delete nnz_recv_temp;
+      delete[] nnz_recv_temp;
 
       // get the number of nnzs to be received from each processor
 
@@ -1465,7 +1465,7 @@ namespace oomph
           nnz_recv[i][p] = nnz_recv_temp[(i * nproc) + p];
         }
       }
-      delete nnz_recv_temp;
+      delete[] nnz_recv_temp;
 
       // get the number of nnzs to be received from each processor
 
