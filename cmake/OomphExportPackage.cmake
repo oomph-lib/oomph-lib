81d118429fa96ab3c26a9d73286b4de308ef645d
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2021-09-18 12:04:09 +0100

    Tidy up variable name choices in CMake scripts.

--- cmake/OomphExportPackage.cmake
+++ cmake/OomphExportPackage.cmake
@@ -1,6 +1,9 @@
 # =============================================================================
 # Create config files that can be included by other projects to find and use
 # oomph-lib using find_package(...) or find_package(... CONFIG).
+#
+# NOTE: A number of the key OOMPH_... values referenced here will have been
+# defined in OomphConfigureProjectEnvironment.cmake.
 # =============================================================================
 # Introduce the variables CMAKE_INSTALL_{LIBDIR,BINDIR,INCLUDEDIR} and functions
 # write_basic_package_version_file(...) and configure_package_config_file(...)
@@ -19,32 +22,31 @@ get_directory_property(
 
 # Configure 'oomphlibConfig.cmake' using 'oomphlibConfig.cmake.in'
 configure_package_config_file(
-  ${OOMPH_CMAKE_CONFIG_TEMPLATE} ${OOMPH_CMAKE_PROJECT_CONFIG_FILE}
-  INSTALL_DESTINATION ${OOMPH_CONFIG_INSTALL_DIR})
+  ${OOMPH_CMAKE_CONFIG_FILE_TEMPLATE} ${OOMPH_CMAKE_CONFIG_FILE}
+  INSTALL_DESTINATION ${OOMPH_INSTALL_CONFIG_DIR})
 
 # Configure 'oomphlibConfigVersion.cmake' which exports the version info
 write_basic_package_version_file(
-  ${OOMPH_CMAKE_VERSION_CONFIG_FILE}
+  ${OOMPH_CMAKE_CONFIG_VERSION_FILE}
   VERSION ${${PROJECT_NAME}_VERSION}
   COMPATIBILITY SameMajorVersion)
 
 # Add all targets to the build-tree export set
 export(
-  EXPORT ${OOMPH_TARGETS_EXPORT_NAME}
+  EXPORT ${OOMPH_EXPORTS_TARGET_NAME}
   NAMESPACE ${PROJECT_NAMESPACE}::
-  FILE ${OOMPH_CMAKE_PROJECT_EXPORTS_FILE})
+  FILE ${OOMPH_CMAKE_EXPORTS_FILE})
 
 # Install config files
-install(FILES ${OOMPH_CMAKE_PROJECT_CONFIG_FILE}
-              ${OOMPH_CMAKE_VERSION_CONFIG_FILE}
-        DESTINATION ${OOMPH_CONFIG_INSTALL_DIR})
+install(FILES ${OOMPH_CMAKE_CONFIG_FILE} ${OOMPH_CMAKE_CONFIG_VERSION_FILE}
+        DESTINATION ${OOMPH_INSTALL_CONFIG_DIR})
 
 # Install the export targets file
 install(
-  EXPORT ${OOMPH_TARGETS_EXPORT_NAME}
+  EXPORT ${OOMPH_EXPORTS_TARGET_NAME}
   NAMESPACE ${PROJECT_NAMESPACE}::
-  FILE "${OOMPH_TARGETS_EXPORT_NAME}.cmake"
-  DESTINATION ${OOMPH_CONFIG_INSTALL_DIR})
+  FILE "${OOMPH_EXPORTS_TARGET_NAME}.cmake"
+  DESTINATION ${OOMPH_INSTALL_CONFIG_DIR})
 
 # Install pkg-config
 configure_file("${CMAKE_CURRENT_LIST_DIR}/${PROJECT_NAME}.pc.in"
@@ -60,14 +62,15 @@ install(FILES "${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}.pc"
 set(CMAKE_MODULES_TO_INSTALL OomphAddExecutable.cmake OomphAddTest.cmake
     OomphEnableCodeCoverage.cmake)
 
-# Place each module in the appropriate build/install directory
+# Place each module in the same build/install directory that we place the
+# oomphlibConfig.cmake file
 foreach(MODULE IN LISTS CMAKE_MODULES_TO_INSTALL)
-  # Copy to binary (i.e. build) directory
   file(COPY "${CMAKE_CURRENT_LIST_DIR}/${MODULE}"
        DESTINATION ${OOMPH_CMAKE_CONFIG_DIR})
 
   # Install it
   install(FILES "${CMAKE_CURRENT_LIST_DIR}/${MODULE}"
-          DESTINATION ${OOMPH_CONFIG_INSTALL_DIR})
+          DESTINATION ${OOMPH_INSTALL_CONFIG_DIR})
 endforeach()
+
 # ------------------------------------------------------------------------------
