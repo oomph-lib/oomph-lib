6e686b7932838e901bc83eb03ef0ddcf8aa18cc0
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2021-10-06 10:39:56 +0100

    Actually add the modified files...

--- bin/update_doxyfile.bash
+++ bin/update_doxyfile.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
 
 # Update all Doxyfiles
-find ../doc -name Doxyfile -exec dirname {} \;
+
+# Find 'em
+dir_list=`find . -name Doxyfile -exec dirname {} \;`
+home_dir=`pwd`
+for dir in `echo $dir_list`; do
+    cd $dir
+    echo "Updating Doxyfile in " `pwd`
+    #cp Doxyfile Doxyfile.tmp
+    #sed 's/a4wide/a4/g' Doxyfile.tmp > Doxyfile
+    #rm Doxyfile.tmp
+    doxygen -u
+    cd $home_dir
+done
 
