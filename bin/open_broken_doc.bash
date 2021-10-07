c1578562ff96af85192439412df866cca5ed5ad1
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2021-10-07 16:08:15 +0100

    Annotate open_broken_doc.bash script and add stuff to .gitignore

--- bin/open_broken_doc.bash
+++ bin/open_broken_doc.bash
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+echo " "
+echo "===================================================="
+echo "I'm going to use the script "
+echo " "
+echo "    bin/find_missing_doxygen_hooks.sh "
+echo " "
+echo "to find potentially broken documentation. "
+echo "For each potentially broken html file, I'll display"
+echo "the file (using an in-script specificable brower)"
+echo "and try to open the *.txt that's likely be the source"
+echo "for the documentation. If it can't be found that way,"
+echo "you'll have to dig around yourself."
+echo " "
+echo "If you need the corresponding source code, follow the"
+echo "link at the bottom of the html file in the browser"
+echo "(for most tutorials)."
+echo "======================================================"
+
 # Specify browser
 browser=firefox
 
@@ -15,9 +33,21 @@ for file in `cat broken_files.txt`; do
     txt_file_dir=`dirname $file`/..
     txt_file=`echo $txt_file_dir/*.txt`
     if [ -e $txt_file ] ; then
+        echo "I'm assuming that the dodgy-looking html code"
+        echo " "
+        echo "        "$file
+        echo " "
+        echo "has been created from "
+        echo " "
+        echo "        "$txt_file
+        echo " "
         firefox $file
         emacs $txt_file
     else
-        echo "No txt file in $txt_file_dir; you'll have to find that file yourself."
+        echo " "
+        echo "No *.txt file in $txt_file_dir; you'll have to find"
+        echo "the txt file that was used to create this documentation"
+        echo "yourself."
+        echo " "
     fi
 done
