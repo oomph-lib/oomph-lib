cf777c4b9fb39904fb094e222533f811125d959c
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-09-29 21:18:35 +0100

    Add demo driver (third_party/test_nlohmann_json/) for nlohmann/json.

---
+++ demo_drivers/third_party/test_nlohmann_json/validate.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+# Get the OOPMH-LIB root directory from a makefile
+OOMPH_ROOT_DIR=$1
+
+#Set the number of tests to be checked
+NUM_TESTS=1
+
+# Setup validation directory
+#---------------------------
+touch Validation
+rm -r -f Validation
+mkdir Validation
+
+# Validation for demo poisson
+#----------------------------
+cd Validation
+
+echo "Running nlohmann/json validation "
+mkdir RESLT
+cd RESLT
+../../test_nlohmann_json >../OUTPUT_test_nlohmann_json || {
+  echo 'Failed to run ./test_nlohmann_json!'
+  exit 1
+}
+cd ..
+echo "done"
+echo " " >>validation.log
+echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >>validation.log
+
+# Append output to global validation log file
+#--------------------------------------------
+cat validation.log >>$OOMPH_ROOT_DIR/validation.log || {
+  echo 'Failed to append validation.log to global validation.log!'
+  exit 1
+}
+
+cd ..
+
+#######################################################################
+
+#Check that we get the correct number of OKs
+# validate_ok_count will exit with status
+# 0 if all tests has passed.
+# 1 if some tests failed.
+# 2 if there are more 'OK' than expected.
+. $OOMPH_ROOT_DIR/scripts/validate_ok_count
+
+# Never get here
+exit 0
