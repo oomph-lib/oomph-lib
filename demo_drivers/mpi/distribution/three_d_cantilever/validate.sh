d51cff8c920091ef8cadd8c68262ab47970cea5b
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-10-30 15:55:12 +0000

    Tweak mpi/distribution/three_d_cantilever/ fpdiff tolerance (small=5.0e-07 -> 5.0e-06).

--- demo_drivers/mpi/distribution/three_d_cantilever/validate.sh
+++ demo_drivers/mpi/distribution/three_d_cantilever/validate.sh
@@ -71,7 +71,7 @@ if test "$3" = "no_fpdiff"; then
   echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
 else
 $OOMPH_ROOT_DIR/scripts/fpdiff.py ../validata/three_d_cantilever_results.dat.gz  \
-         three_d_cantilever_results.dat 0.2 5.0e-7 >> validation.log
+         three_d_cantilever_results.dat 0.2 5.0e-06 >> validation.log
 fi
 
 mkdir RESLT_three_d_cantilever_adapt
