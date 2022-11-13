d1fc01328d02b11189554250d1f70d60af0a2398
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-11-13 18:30:19 +0000

    Remove tests/ directory and cmake/OomphGetDoctest.cmake.

--- cmake/OomphGetDoctest.cmake
+++
@@ -1,9 +0,0 @@
-# =============================================================================
-# Download the unit-testing suite, doctest.
-# =============================================================================
-include(FetchContent)
-FetchContent_Declare(
-  doctest_project
-  GIT_REPOSITORY "https://github.com/onqtam/doctest.git"
-  GIT_TAG 2.4.6)
-FetchContent_MakeAvailable(doctest_project)
