0b27ff33e4bd47b140c0b7ac727d89b83b5d2259
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-12-14 19:43:35 +0000

    Integrate self_test/deliberately_broken_code_for_self_test_test/ and self_test/generic/block_selector_test into CMake framework.

--- self_test/deliberately_broken_code_for_self_test_test/validate.sh
+++ self_test/deliberately_broken_code_for_self_test_test/validate.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
 
 # Get the OOMPH-LIB root directory from a makefile
-OOMPH_ROOT_DIR=$(make -s --no-print-directory print-top_builddir)
-
+OOMPH_ROOT_DIR=$1
 
 #Set the number of tests to be checked
 NUM_TESTS=1
@@ -18,51 +17,39 @@ mkdir Validation
 cd Validation
 
 echo "Running 'code with warning' "
-../code_with_warning > OUTPUT
+../code_with_warning >OUTPUT
 echo "done"
-echo " " >> validation.log
-echo "'Code with warning' validation" >> validation.log
-echo "----------------------------" >> validation.log
-echo " " >> validation.log
-echo "Validation directory: " >> validation.log
-echo " " >> validation.log
-echo "  " `pwd` >> validation.log
-echo " " >> validation.log
-cat  OUTPUT \
- > results.dat
+echo " " >>validation.log
+echo "'Code with warning' validation" >>validation.log
+echo "----------------------------" >>validation.log
+echo " " >>validation.log
+echo "Validation directory: " >>validation.log
+echo " " >>validation.log
+echo "  " $(pwd) >>validation.log
+echo " " >>validation.log
+cat OUTPUT \
+  >results.dat
 
 if test "$1" = "no_fpdiff"; then
-  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >> validation.log
+  echo "dummy [OK] -- Can't run fpdiff.py because we don't have python or validata" >>validation.log
 else
-../../../bin/fpdiff.py ../validata/results.dat.gz  \
-         results.dat >> validation.log
+  $OOMPH_ROOT_DIR/scripts/fpdiff.py ../validata/results.dat.gz \
+    results.dat >>validation.log
 fi
 
 # Append log to main validation log
-cat validation.log >> ../../../validation.log
-
+cat validation.log >>$OOMPH_ROOT_DIR/validation.log
 
 cd ..
 
-
-# Try to compile broken code to create error message
-#---------------------------------------------------
-# (code doesn't need any Makefile.am instructions)
-#-------------------------------------------------
-make -k deliberately_broken_code
-
-
-
-
 #######################################################################
-
 
 #Check that we get the correct number of OKs
 # validate_ok_count will exit with status
 # 0 if all tests has passed.
 # 1 if some tests failed.
 # 2 if there are more 'OK' than expected.
-. $OOMPH_ROOT_DIR/bin/validate_ok_count
+. $OOMPH_ROOT_DIR/scripts/validate_ok_count
 
 # Never get here
 exit 10
