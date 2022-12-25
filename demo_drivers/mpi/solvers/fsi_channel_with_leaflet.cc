a53e4ee7fdca3dad24b0e2bf1c630307b853dddc
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2022-12-25 14:12:02 +0000

    Replace with sprintf() with snprintf() in mpi/solvers/fsi_channel_with_leaflet.cc.

--- demo_drivers/mpi/solvers/fsi_channel_with_leaflet.cc
+++ demo_drivers/mpi/solvers/fsi_channel_with_leaflet.cc
@@ -558,14 +558,14 @@ void FSIChannelWithLeafletProblem<ELEMENT>::doc_solution(DocInfo& doc_info)
  npts=5; 
 
  // Output fluid solution 
- sprintf(filename,"%s/fsi_fluid_soln%i.dat",doc_info.directory().c_str(),
+ snprintf(filename,sizeof(filename),"%s/fsi_fluid_soln%i.dat",doc_info.directory().c_str(),
          doc_info.number());
  some_file.open(filename);
  Fluid_mesh_pt->output(some_file,npts);
  some_file.close();
 
  // Output wall solution 
- sprintf(filename,"%s/fsi_wall_soln%i.dat",doc_info.directory().c_str(),
+ snprintf(filename,sizeof(filename),"%s/fsi_wall_soln%i.dat",doc_info.directory().c_str(),
          doc_info.number());
  some_file.open(filename);
  Solid_mesh_pt->output(some_file,npts);
@@ -577,7 +577,7 @@ void FSIChannelWithLeafletProblem<ELEMENT>::doc_solution(DocInfo& doc_info)
  // Output fluid elements on fluid mesh boundary 4 (associated with
  // the "front")
  unsigned bound=4;
- sprintf(filename,"%s/fluid_boundary_elements_front_%i.dat",
+ snprintf(filename,sizeof(filename),"%s/fluid_boundary_elements_front_%i.dat",
          doc_info.directory().c_str(),
          doc_info.number());
  some_file.open(filename);
@@ -593,7 +593,7 @@ void FSIChannelWithLeafletProblem<ELEMENT>::doc_solution(DocInfo& doc_info)
  // Output fluid elements on fluid mesh boundary 5 (associated with
  // the "back")
  bound=5;
- sprintf(filename,"%s/fluid_boundary_elements_back_%i.dat",
+ snprintf(filename,sizeof(filename),"%s/fluid_boundary_elements_back_%i.dat",
          doc_info.directory().c_str(),
          doc_info.number());
  some_file.open(filename);
@@ -607,7 +607,7 @@ void FSIChannelWithLeafletProblem<ELEMENT>::doc_solution(DocInfo& doc_info)
 
 
  // Output normal vector on wall elements
- sprintf(filename,"%s/wall_normal_%i.dat",
+ snprintf(filename,sizeof(filename),"%s/wall_normal_%i.dat",
          doc_info.directory().c_str(),
          doc_info.number());
  some_file.open(filename);
