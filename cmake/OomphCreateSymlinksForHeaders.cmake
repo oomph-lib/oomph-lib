823c1cd4fe0a1e978fc16f56a2c9d423f181bf2d
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2023-01-02 13:50:11 +0000

    Move from old-school get_filename_component(...) to the more modern cmake_path(...) command.

--- cmake/OomphCreateSymlinksForHeaders.cmake
+++ cmake/OomphCreateSymlinksForHeaders.cmake
@@ -1,12 +1,18 @@
 # cmake-format: off
 # =============================================================================
+# DESCRIPTION:
+# ------------
 # Create symlinks for headers from the build/include directory to the *actual*
 # location of the to-be-installed header files.
 #
-# NOTE: We have to be careful to use "install(...)" commands only as we can only
-# create the actual links at install time.
+# NOTE (1): We have to be careful to use "install(...)" commands only as we can
+# only create the actual links at install time.
 #
-# Usage:
+# NOTE (2): The symlink destination MUST(!) be relative to the installation
+# directory.
+#
+# USAGE:
+# ------
 #
 #  include(OomphCreateSymlinksForHeaders)
 #  oomph_create_symlinks_for_headers(REAL_DIR    <dir-to-symlink-to>
@@ -29,16 +35,18 @@ function(oomph_create_symlinks_for_headers)
                         "${SINGLE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
 
   # Symlink the headers
-  foreach(HEADER ${${PREFIX}_HEADERS})
-    get_filename_component(HEADER_PATH ${HEADER} DIRECTORY)
+  foreach(HEADER IN LISTS ${PREFIX}_HEADERS)
+    # Check whether HEADER contains just a filename (e.g. "file.h") or a
+    # filename with a parent path (e.g. "include/file.h")
+    cmake_path(HAS_PARENT_PATH HEADER HAS_PARENT_PATH_COMPONENT)
 
     # If the header is an absolute path (i.e. the DIRECTORY component is
     # non-empty), link it directly. Otherwise, use REAL_DIR as the base
     # directory
-    if(HEADER_PATH)
-      get_filename_component(HEADER_NAME ${HEADER} NAME)
+    if(HAS_PARENT_PATH_COMPONENT)
+      cmake_path(GET HEADER FILENAME HEADER_FILENAME)
       set(TARGET "${HEADER}")
-      set(LINK "${${PREFIX}_SYMLINK_DIR}/${HEADER_NAME}")
+      set(LINK "${${PREFIX}_SYMLINK_DIR}/${HEADER_FILENAME}")
     else()
       set(TARGET "${${PREFIX}_REAL_DIR}/${HEADER}")
       set(LINK "${${PREFIX}_SYMLINK_DIR}/${HEADER}")
