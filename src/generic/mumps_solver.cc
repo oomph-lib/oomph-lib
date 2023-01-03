3da3e21693594944a6d6ffa79142cd1c3d556437
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2023-01-03 18:07:03 +0000

    Patch src/generic/mumps_solver.cc so that MUMPS can be used in serial.

--- src/generic/mumps_solver.cc
+++ src/generic/mumps_solver.cc
@@ -668,12 +668,14 @@ namespace oomph
       tmp_rhs[j] = Mumps_struc_pt->rhs[j];
     }
 
+#ifdef OOMPH_HAS_MPI
     // Broadcast the result which is only held on root
     MPI_Bcast(&tmp_rhs[0],
               ndof,
               MPI_DOUBLE,
               0,
               this->distribution_pt()->communicator_pt()->mpi_comm());
+#endif
 
     // If the result vector is distributed, re-distribute the
     // non-distributed tmp_rhs vector to match
