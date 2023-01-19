6c37ba3d70520824be6d9db12c622a3bba177bd4
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2023-01-19 23:51:23 +0000

    Patch how compile definitions are added; only add them to C++ files. Fixes the build of Scalapack when OOMPH_ENABLE_PARANOID is enabled.

---
+++ cmake/OomphAddCXXCompileDefinitions.cmake
@@ -0,0 +1,28 @@
+# cmake-format: off
+# =============================================================================
+# DESCRIPTION:
+# ------------
+# Helper to define compiler definitions for C++ files only.
+#
+# USAGE:
+# ------
+#     oomph_add_cxx_compile_definitions(<CXX-DEFINITION>)
+#     oomph_add_cxx_compile_definitions(${<LIST-OF-CXX-DEFINITIONS>})
+#
+# EXAMPLE:
+# --------
+#     # Add -DPARANOID compiler definition when CMake is built in Debug mode
+#     oomph_add_cxx_compile_definitions($<$<CONFIG:DEBUG,>:PARANOID>)
+#
+#     # Add -DOOMPH_HAS_MPI compiler definition
+#     oomph_add_cxx_compile_definitions(OOMPH_HAS_MPI)
+# =============================================================================
+# cmake-format: on
+include_guard()
+
+function(oomph_add_cxx_compile_definitions)
+  foreach(CXX_DEFINITION IN LISTS ARGN)
+    add_compile_definitions($<$<COMPILE_LANGUAGE:CXX>:${CXX_DEFINITION}>)
+  endforeach()
+endfunction()
+# ------------------------------------------------------------------------------
