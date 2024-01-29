a4936f1339e7cb0ab9e6a2fcec4ecb0e21fd6de3
Paul Alexander <paulalexander23@live.co.uk>
2024-01-29 10:59:07 +0000

    Remove the this-> to allow for function overloading?

--- src/generic/elements.h
+++ src/generic/elements.h
@@ -1037,7 +1037,7 @@ namespace oomph
       // Zero the dres_dparam vector
       dres_dparam.initialise(0.0);
       // Add the elemental contribution to the residuals vector
-      this->fill_in_contribution_to_dresiduals_dparameter(parameter_pt,
+      fill_in_contribution_to_dresiduals_dparameter(parameter_pt,
                                                           dres_dparam);
     }
 
