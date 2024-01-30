ac23aa3017f76c8e64790359a8cbdc36daedd85a
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2024-01-30 13:51:08 +0000

    Update include headers in superlu_dist.c.

--- src/generic/superlu_dist.c
+++ src/generic/superlu_dist.c
@@ -24,18 +24,16 @@
   ----------------------------------------------------------------
 */
 #include <math.h>
-#ifdef USING_OOMPH_SUPERLU_DIST
-#include "oomph_superlu_dist_8.2.1.h"
-#else
 #include <superlu_defs.h>
 #include <superlu_ddefs.h>
 #include <slu_Cnames.h>
 #include <machines.h>
 #include <psymbfact.h>
 #include <supermatrix.h>
-#include <old_colamd.h>
+// #include <old_colamd.h>
+#include <superlu_dist_config.h>
+#include <superlu_enum_consts.h>
 #include <util_dist.h>
-#endif
 
 
 /* ================================================= */
