7599e0567ca78e5f2011b1ea2c8c9603c8128275
Paul Alexander <paulalexander23@live.co.uk>
2024-11-23 14:15:05 +0000

    Bug fix. Remove adding scaling twice.

--- demo_drivers/navier_stokes/obtuse_corner_flow/singular_sector_problem.h
+++ demo_drivers/navier_stokes/obtuse_corner_flow/singular_sector_problem.h
@@ -488,8 +488,6 @@ namespace oomph
     }
     PointPressureEvaluationElement* el_pt =
       new PointPressureEvaluationElement(node_pt);
-    el_pt->set_pressure_data_pt(
-      Singularity_scaling_mesh_pt->element_pt(0)->internal_data_pt(0));
 
     el_pt->set_pressure_data_pt(
       Singularity_scaling_mesh_pt->element_pt(0)->internal_data_pt(0));
