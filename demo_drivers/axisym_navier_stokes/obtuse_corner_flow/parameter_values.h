a0b965f6889937660d316320d17afd8fb7bb39bd
Paul Alexander <paulalexander23@live.co.uk>
2024-11-25 16:16:57 +0000

    update parameter_values.h

--- demo_drivers/axisym_navier_stokes/obtuse_corner_flow/parameter_values.h
+++ demo_drivers/axisym_navier_stokes/obtuse_corner_flow/parameter_values.h
@@ -7,15 +7,15 @@ namespace oomph
 {
   namespace parameters
   {
-    double contact_angle = 2.0 / 3.0 * MathematicalConstants::Pi;
-    const double slip_length = 1e0;
+    double contact_angle = 135.0 / 180.0 * MathematicalConstants::Pi;
+    const double slip_length = 1e-1;
     double capillary_number = 1e0;
     const double element_area = 1e-2;
     double nu = 0.25;
     double reynolds_number = 0.0;
-    double strouhal_reynolds_number = 1.0;
+    double strouhal_reynolds_number = 0.0;
     const double small_r = 1e-4;
-    const double inner_radius = 0.001;
+    const double inner_radius = 1e-5;
     const unsigned nsegment = 32;
   } // namespace parameters
 } // namespace oomph
