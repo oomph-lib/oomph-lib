1d38737dc1c50ad8b9a4098e68aa45647af194a3
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2025-02-12 16:40:36 +0000

    Update OomphAddTest.cmake and OomphDefineTestData.cmake to silence policy CMP0175 warnings.

--- cmake/OomphAddTest.cmake
+++ cmake/OomphAddTest.cmake
@@ -172,7 +172,7 @@ function(oomph_add_test)
     # To silence the warning about a missing COMMAND key in the
     # add_custom_command(...) command below. We're going to just enable the
     # policy locally (by pushing to the policy stack then popping after)
-    if(POLICY CMP1234)
+    if(POLICY CMP0175)
       cmake_policy(PUSH)
       cmake_policy(VERSION 3.31)
       cmake_policy(SET CMP0175 OLD)
@@ -185,7 +185,7 @@ function(oomph_add_test)
       POST_BUILD
       BYPRODUCTS ${TEST_BYPRODUCTS})
 
-    if(POLICY CMP1234)
+    if(POLICY CMP0175)
       # Remove the policy enabled above
       cmake_policy(POP)
     endif()
