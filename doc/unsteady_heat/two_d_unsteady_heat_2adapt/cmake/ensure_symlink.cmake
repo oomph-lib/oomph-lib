cb6d67ba6b1d28a6033c3af0b69067cb861a221a
Puneet Matharu <puneet.matharu@hotmail.co.uk>
2025-06-07 13:22:55 +0100

    Update symlinking approach in doc/*/two_d_unsteady_heat_2adapt/.

---
+++ doc/unsteady_heat/two_d_unsteady_heat_2adapt/cmake/ensure_symlink.cmake
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------------------------
+# Usage:  cmake -P ensure_symlink.cmake <SOURCE> <DEST>
+# ------------------------------------------------------------------------------
+if(NOT "${CMAKE_ARGC}" EQUAL 5)
+  message(FATAL_ERROR "Usage: ensure_symlink.cmake <SOURCE> <DEST>")
+endif()
+
+math(EXPR SRC_IDX "${CMAKE_ARGC} - 2") # index of second-to-last argument
+math(EXPR DST_IDX "${CMAKE_ARGC} - 1") # index of last argument
+
+set(SRC "${CMAKE_ARGV${SRC_IDX}}")
+set(DST "${CMAKE_ARGV${DST_IDX}}")
+
+if(NOT EXISTS "${DST}")
+  # No file yet so create a link
+  execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink "${SRC}" "${DST}")
+elseif(NOT IS_SYMLINK "${DST}")
+  # Exists but is a regular file/dir so remove + link
+  file(REMOVE_RECURSE "${DST}")
+  execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink "${SRC}" "${DST}")
+else()
+  file(READ_SYMLINK "${DST}" CURRENT_TARGET)
+  if(NOT CURRENT_TARGET STREQUAL "${SRC}")
+    # Points somewhere else so relink
+    file(REMOVE "${DST}")
+    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink "${SRC}"
+                            "${DST}")
+  endif()
+endif()
+# ------------------------------------------------------------------------------
