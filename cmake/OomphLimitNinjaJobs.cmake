8fa743aa842c766ae87d243c475ccfd92830db7a
Matthias Heil <M.Heil@maths.manchester.ac.uk>
2026-01-02 17:32:20 +0000

    final (hopefully) revision of ninja limiter; CMakeLists.txt file in demo_driver passes info on to underlying directories

--- cmake/OomphLimitNinjaJobs.cmake
+++ cmake/OomphLimitNinjaJobs.cmake
@@ -52,9 +52,24 @@ function(oomph_limit_ninja_jobs)
         return()
     endif()
 
+    # Check if the number of parallel jobs has already been set in the environment;
+    # if so, respect this and bail.
+    if (DEFINED ENV{CMAKE_BUILD_PARALLEL_LEVEL})
+       set(_tmp "$ENV{CMAKE_BUILD_PARALLEL_LEVEL}")
+       message(STATUS
+         "oomph_limit_ninja_jobs(): CMAKE_BUILD_PARALLEL_LEVEL is set to "
+         "(${_tmp}); skipping Ninja job limiting")
+       return()
+    endif()
+
     # Define a global job pool (but labeled, to avoid clashes)
     set(_oomph_pool oomph_limit_pool)
-    set_property(GLOBAL PROPERTY JOB_POOLS ${_oomph_pool}=${NJOBS})
+
+    # Define the pool exactly once (defensive against leakage / reuse)
+    get_property(_pools GLOBAL PROPERTY JOB_POOLS)
+    if (NOT _pools MATCHES "(^|;)${_oomph_pool}=")
+      set_property(GLOBAL PROPERTY JOB_POOLS ${_oomph_pool}=${NJOBS})
+    endif()
 
     # Apply to all job types (COMPILE, LINK, CUSTOM) unless
     # limits are already set by user
@@ -63,8 +78,25 @@ function(oomph_limit_ninja_jobs)
           set(CMAKE_JOB_POOL_${kind} ${_oomph_pool} PARENT_SCOPE)
       endif()
     endforeach()
+    
+    message(STATUS
+    "The call to oomph_limit_ninja_jobs() from the current project's CMakeLists.txt\n"
+    "    file limits Ninja's parallelism via a job pool with depth=${NJOBS}, so\n"
+    "    the subsequent build will use at most ${NJOBS} parallel jobs. \n"
+    "    Note: Passing '-j' to 'cmake --build' or 'ninja' will NOT increase this\n"
+    "    but can be used to reduce the number of parallel jobs further.\n"
+    "    To override, you can globally set the number of parallel build jobs via the\n"
+    "    environment variable CMAKE_BUILD_PARALLEL_LEVEL prior to configuring, i.e. do\n"
+    "       CMAKE_BUILD_PARALLEL_LEVEL=<N>\n"
+    "    on the command line.\n"
+    "    Alternatively, when configuring oomph-lib itself you can disable the limiter\n"
+    "    by configuring oomph-lib using \n"
+    "       cmake -G Ninja [...] -DOOMPH_DO_NOT_LIMIT_NINJA_PARALLEL_JOBS=ON\n"
+    "    In that case Ninja uses its aggressive default of njobs = number of processors + 2\n"
+    "    to build the project. Most computers can handle this but some die, so use\n"
+    "    with caution."
+    )   
 
-    message(STATUS "oomph-lib has limited Ninja parallel jobs to ${NJOBS}")
 endfunction()
 
 message(VERBOSE "Finished limiting number of ninja jobs")
